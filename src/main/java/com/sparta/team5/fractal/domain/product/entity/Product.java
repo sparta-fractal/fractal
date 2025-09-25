6e2764307e6ad2a1be9620d66eac7f08f4fd2042
Kuandyk Kaiyrzhan <kuandykkaiyrzhan@gmail.com>
2025-09-25 22:02:53 +0900

    feat/product: init settings

---
+++ src/main/java/com/sparta/team5/fractal/domain/product/entity/Product.java
@@ -0,0 +1,6 @@
+package com.sparta.team5.fractal.domain.product.entity;
+
+import com.sparta.team5.fractal.common.entity.BaseEntity;
+
+public class Product extends BaseEntity {
+}
---
+++ src/main/java/com/sparta/team5/fractal/domain/product/service/ProductService.java
@@ -0,0 +1,4 @@
+package com.sparta.team5.fractal.domain.product.service;
+
+public class ProductService {
+}
