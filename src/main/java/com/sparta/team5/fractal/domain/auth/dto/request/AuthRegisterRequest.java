6c334264e4745beaa711ff9967603e5898ddff11
AllSungho <nanumeran@gmail.com>
2025-09-26 02:17:00 +0900

    refactor/auth: request message change

--- src/main/java/com/sparta/team5/fractal/domain/auth/dto/request/AuthRegisterRequest.java
+++ src/main/java/com/sparta/team5/fractal/domain/auth/dto/request/AuthRegisterRequest.java
@@ -17,8 +17,8 @@ public record AuthRegisterRequest(
         @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]).*$", message = "비밀번호는 영문, 숫자, 특수문자를 모두 포함해야 합니다.")
         String password,
 
-        @NotBlank(message = "이름은 필수입니다.")
-        @Size(min = 2, max = 20, message = "이름은 2자 이상 20자 이하로 입력해주세요.")
+        @NotBlank(message = "닉네임은 필수입니다.")
+        @Size(min = 2, max = 20, message = "닉네임은 2자 이상 20자 이하로 입력해주세요.")
         String nickname
 ) {
 }
