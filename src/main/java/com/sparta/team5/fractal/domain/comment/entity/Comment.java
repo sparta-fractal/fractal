bdeba666e79ef8f4fa3a6c963c90e3bc6b1e0168
baek_do_hyeon <ehgus6666@naver.com>
2025-09-25 20:53:50 +0900

    feat/comment: comment init setting

--- src/main/java/com/sparta/team5/fractal/domain/comment/entity/Comment.java
+++ src/main/java/com/sparta/team5/fractal/domain/comment/entity/Comment.java
@@ -9,8 +9,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 
 @Entity
 @NoArgsConstructor
@@ -27,14 +29,19 @@ public class Comment {
     @JoinColumn(name = "userId", nullable = false)
     private User user; //작성자
 
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
 //    @ManyToOne(fetch = FetchType.LAZY)
 //    @JoinColumn(name = "parentId")
 //    private Product product;
 //
 //
-//    public Comment(String content, User user, Product product) {
-//        this.content = content;
-//        this.user = user;
-//        this.product = product;
-//    }
+
+    public Comment(String content, User user, LocalDateTime createdAt) {
+        this.content = content;
+        this.user = user;
+        this.createdAt = createdAt;
+    }
 }
--- src/main/java/com/sparta/team5/fractal/domain/comment/service/CommentService.java
+++ src/main/java/com/sparta/team5/fractal/domain/comment/service/CommentService.java
@@ -1,5 +1,7 @@
 package com.sparta.team5.fractal.domain.comment.service;
 
+import org.springframework.stereotype.Service;
 
+@Service
 public class CommentService implements CommentServiceApi {
 }
