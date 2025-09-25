e678cfc8d5ea64ec03a2591a0a3ab6ccaa41cd72
Sungho <nanumeran@gmail.com>
2025-09-25 22:15:15 +0900

    feat/auth: signup (#28)
    * feat/auth: JWT init settings
    
    * feat/auth: signup implementation
    
    * refactor/auth: Jwt refactoring
    
    * refactor/auth: Jwt, LoginRequest

---
+++ src/main/java/com/sparta/team5/fractal/domain/auth/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.sparta.team5.fractal.domain.auth.controller;
+
+import com.sparta.team5.fractal.common.response.ApiResponse;
+import com.sparta.team5.fractal.domain.auth.dto.request.AuthRegisterRequest;
+import com.sparta.team5.fractal.domain.auth.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<ApiResponse<Void>> register(
+            @Valid @RequestBody AuthRegisterRequest authRegisterRequest
+    ) {
+
+        authService.register(authRegisterRequest);
+
+        return ApiResponse.success(null, "회원가입 성공");
+    }
+}
---
+++ src/main/java/com/sparta/team5/fractal/domain/auth/dto/respone/AuthResponse.java
@@ -0,0 +1,4 @@
+package com.sparta.team5.fractal.domain.auth.dto.respone;
+
+public record AuthResponse(String token) {
+}
