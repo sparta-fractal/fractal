d431a810f2465b289ffad0fafd2a3f62d2b5e773
MinHyeongK <syjjmh1@naver.com>
2025-09-25 10:51:44 +0900

    chore/basic: skeleton code

---
+++ src/test/java/com/sparta/team5/fractal/FractalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.team5.fractal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FractalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
