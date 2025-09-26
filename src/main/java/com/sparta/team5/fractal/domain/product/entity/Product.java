010f82d2dde46db1a55c34124dd5e3ec606c02b5
Kuandyk Kaiyrzhan <kuandykkaiyrzhan@gmail.com>
2025-09-26 09:27:35 +0900

    feat/product: implement Product entity

--- src/main/java/com/sparta/team5/fractal/domain/product/entity/Product.java
+++ src/main/java/com/sparta/team5/fractal/domain/product/entity/Product.java
@@ -1,6 +1,46 @@
 package com.sparta.team5.fractal.domain.product.entity;
 
 import com.sparta.team5.fractal.common.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "products")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class Product extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false, length = 50)
+    private String price;
+
+    @Column(nullable = false, length = 1000)
+    private String description;
+
+    @Column(nullable = false, length = 200)
+    private String tags;
+
+    private Product(String title, String price, String description, String tags) {
+        this.title = title;
+        this.price = price;
+        this.description = description;
+        this.tags = tags;
+    }
+
+    public static Product of(String title, String price, String description, String tags) {
+        return new Product(title, price, description, tags);
+    }
 }
