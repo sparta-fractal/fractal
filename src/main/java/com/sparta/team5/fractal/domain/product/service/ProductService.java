2123cd0c3a25f6a4ea440e23a21591741f9a92ce
Kuandyk Kaiyrzhan <kuandykkaiyrzhan@gmail.com>
2025-09-26 09:29:15 +0900

    feat/product: implement createProduct function in ProductService

--- src/main/java/com/sparta/team5/fractal/domain/product/service/ProductService.java
+++ src/main/java/com/sparta/team5/fractal/domain/product/service/ProductService.java
@@ -1,4 +1,30 @@
 package com.sparta.team5.fractal.domain.product.service;
 
+import com.sparta.team5.fractal.domain.product.dto.ProductCreateRequest;
+import com.sparta.team5.fractal.domain.product.dto.ProductResponse;
+import com.sparta.team5.fractal.domain.product.entity.Product;
+import com.sparta.team5.fractal.domain.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductResponse createProduct(ProductCreateRequest request) {
+        Product product = Product.of(
+            request.title(),
+            request.price(),
+            request.description(),
+            request.tags()
+        );
+
+        Product savedProduct = productRepository.save(product);
+
+        return ProductResponse.from(savedProduct);
+    }
 }
