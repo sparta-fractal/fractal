d92c0f7828f4bae341404c0647cd77b1b2540eb6
baek_do_hyeon <ehgus6666@naver.com>
2025-09-26 13:17:56 +0900

    feat/comment: comment read

--- src/main/java/com/sparta/team5/fractal/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/sparta/team5/fractal/domain/comment/repository/CommentRepository.java
@@ -1,7 +1,11 @@
 package com.sparta.team5.fractal.domain.comment.repository;
 
 import com.sparta.team5.fractal.domain.comment.entity.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    Page<Comment> findAllByProductId(Long productId, Pageable pageable);
 }
