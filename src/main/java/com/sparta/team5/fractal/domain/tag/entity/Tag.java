bc153d45b275beb617ba1d5065e7567569db512c
Kuandyk Kaiyrzhan <kuandykkaiyrzhan@gmail.com>
2025-09-26 19:22:03 +0900

    refactor/tag: extend Tag entity with soft delete

--- src/main/java/com/sparta/team5/fractal/domain/tag/entity/Tag.java
+++ src/main/java/com/sparta/team5/fractal/domain/tag/entity/Tag.java
@@ -1,15 +1,18 @@
 package com.sparta.team5.fractal.domain.tag.entity;
 
+import com.sparta.team5.fractal.common.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Table(name = "tags")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class Tag {
+@SQLRestriction("is_deleted = false")
+public class Tag extends BaseEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,14 +22,14 @@ public class Tag {
     private String name;
 
     @Column(name = "views")
-    private Long views;
+    private Long views = 0L;
 
     private Tag(String name) {
         this.name = name;
         this.views = 0L;
     }
 
-    public static Tag of(String name) {
+    public static Tag from(String name) {
         return new Tag(name);
     }
 }
