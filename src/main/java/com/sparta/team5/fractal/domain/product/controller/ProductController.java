438e36f6d9de1133ed2df996c308db2a5ed19174
Kuandyk <51141147+4x2vk@users.noreply.github.com>
2025-09-26 11:13:59 +0900

    Feat/product find (#37)
    * feat/product: add SQLRestriction for softDelete
    
    * feat/product: implement getProduct in ProductController
    
    * feat/product: implement findById, existsById to find exist product

--- src/main/java/com/sparta/team5/fractal/domain/product/controller/ProductController.java
+++ src/main/java/com/sparta/team5/fractal/domain/product/controller/ProductController.java
@@ -28,4 +28,17 @@ public class ProductController {
 
         return ApiResponse.created(productResponse, "상품이 성공적으로 생성되었습니다.");
     }
+
+    /**
+     * 상품 단건 조회 엔드포인트
+     * 
+     * @param productId 조회할 상품 ID
+     * @return 조회된 상품 정보
+     */
+    @GetMapping("/{productId}")
+    public ResponseEntity<ApiResponse<ProductResponse>> getProduct(@PathVariable Long productId) {
+        ProductResponse productResponse = productService.getProduct(productId);
+
+        return ApiResponse.success(productResponse, "상품 조회에 성공했습니다.");
+    }
 }
