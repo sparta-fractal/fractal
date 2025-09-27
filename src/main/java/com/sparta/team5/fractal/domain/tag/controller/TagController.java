732be79af0dce95a34bfee7065ae03a0ea9364d4
baek_do_hyeon <ehgus6666@naver.com>
2025-09-28 05:59:47 +0900

    feat/tag:search tag-product

--- src/main/java/com/sparta/team5/fractal/domain/tag/controller/TagController.java
+++ src/main/java/com/sparta/team5/fractal/domain/tag/controller/TagController.java
@@ -1,30 +1,37 @@
 package com.sparta.team5.fractal.domain.tag.controller;
 
-import java.util.List;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.sparta.team5.fractal.common.response.ApiResponse;
+import com.sparta.team5.fractal.domain.tag.dto.response.TagProductResponse;
 import com.sparta.team5.fractal.domain.tag.dto.response.TagResponse;
 import com.sparta.team5.fractal.domain.tag.service.TagService;
-
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/tags")
 public class TagController {
 
-	private final TagService tagService;
+    private final TagService tagService;
 
-	@GetMapping
-	public ResponseEntity<ApiResponse<List<TagResponse>>> getAllTags() {
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<TagResponse>>> getAllTags() {
 
-		List<TagResponse> tags = tagService.getAllTags();
+        List<TagResponse> tags = tagService.getAllTags();
 
-		return ApiResponse.success(tags, "태그 목록을 성공적으로 조회하였습니다.");
-	}
+        return ApiResponse.success(tags, "태그 목록을 성공적으로 조회하였습니다.");
+    }
+
+    @GetMapping("/{tagId}")
+    public ResponseEntity<ApiResponse<TagProductResponse>> getTag(@PathVariable Long tagId) {
+
+        TagProductResponse tags = tagService.getTag(tagId);
+
+        return ApiResponse.success(tags, "태그를 조회 하였습니다.");
+    }
 }
--- src/main/java/com/sparta/team5/fractal/domain/tag/service/TagService.java
+++ src/main/java/com/sparta/team5/fractal/domain/tag/service/TagService.java
@@ -1,30 +1,38 @@
 package com.sparta.team5.fractal.domain.tag.service;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
+import com.sparta.team5.fractal.domain.tag.dto.response.TagProductResponse;
 import com.sparta.team5.fractal.domain.tag.dto.response.TagResponse;
 import com.sparta.team5.fractal.domain.tag.entity.Tag;
 import com.sparta.team5.fractal.domain.tag.repository.TagRepository;
-
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class TagService {
 
-	private final TagRepository tagRepository;
+    private final TagRepository tagRepository;
 
-	@Transactional(readOnly = true)
-	public List<TagResponse> getAllTags() {
+    @Transactional(readOnly = true)
+    public List<TagResponse> getAllTags() {
 
-		List<Tag> tags = tagRepository.findAll();
+        List<Tag> tags = tagRepository.findAll();
 
-		return tags.stream()
-			.map(TagResponse::from)
-			.collect(Collectors.toList());
-	}
+        return tags.stream()
+                .map(TagResponse::from)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public TagProductResponse getTag(Long tagId) {
+        Tag tag = tagRepository.findByIdWithProducts(tagId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 태그를 찾을 수 없습니다. id: "));
+
+        tag.increaseViewCount();
+
+        return TagProductResponse.from(tag);
+    }
 }
