cdf2131459edf3afd180e6ffb704c585d9515477
baek_do_hyeon <ehgus6666@naver.com>
2025-09-29 23:51:06 +0900

    feat/category-tag-add-paging

---
+++ src/main/java/com/sparta/team5/fractal/domain/tag/service/TagProductService.java
@@ -0,0 +1,35 @@
+package com.sparta.team5.fractal.domain.tag.service;
+
+import com.sparta.team5.fractal.common.exception.GlobalException;
+import com.sparta.team5.fractal.domain.product.dto.ProductSimpleResponse;
+import com.sparta.team5.fractal.domain.product.entity.Product;
+import com.sparta.team5.fractal.domain.product.service.ProductServiceApi;
+import com.sparta.team5.fractal.domain.tag.dto.response.TagProductResponse;
+import com.sparta.team5.fractal.domain.tag.entity.Tag;
+import com.sparta.team5.fractal.domain.tag.exception.TagErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class TagProductService {
+    private final ProductServiceApi productServiceApi;
+    private final TagServiceApi tagServiceApi;
+
+    @Transactional
+    public TagProductResponse getTag(Long tagId, Pageable pageable) {
+        Tag tag = tagServiceApi.findByIdWithProducts(tagId)
+                .orElseThrow(() -> new GlobalException(TagErrorCode.TAG_NOT_FOUND));
+
+        tag.increaseViewCount();
+
+        Page<Product> productPage = productServiceApi.findProductsByTagId(tagId, pageable);
+
+        Page<ProductSimpleResponse> productDtoPage = productPage.map(ProductSimpleResponse::from);
+
+        return TagProductResponse.from(tag,productDtoPage);
+    }
+}
