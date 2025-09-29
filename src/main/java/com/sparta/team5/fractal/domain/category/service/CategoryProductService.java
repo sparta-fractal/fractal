22d0a2e811874ca9e5610a81d939dedd140af27c
baek_do_hyeon <ehgus6666@naver.com>
2025-09-30 00:07:51 +0900

    feat/category-add-paging

---
+++ src/main/java/com/sparta/team5/fractal/domain/category/service/CategoryProductService.java
@@ -0,0 +1,34 @@
+package com.sparta.team5.fractal.domain.category.service;
+
+import com.sparta.team5.fractal.common.exception.GlobalException;
+import com.sparta.team5.fractal.domain.category.dto.CategoryProductResponse;
+import com.sparta.team5.fractal.domain.category.entity.Category;
+import com.sparta.team5.fractal.domain.category.exception.CategoryErrorCode;
+import com.sparta.team5.fractal.domain.product.dto.ProductSimpleResponse;
+import com.sparta.team5.fractal.domain.product.entity.Product;
+import com.sparta.team5.fractal.domain.product.service.ProductServiceApi;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryProductService {
+    private final ProductServiceApi productServiceApi;
+    private final CategoryServiceApi categoryServiceApi;
+
+    @Transactional(readOnly = true)
+    public CategoryProductResponse getCategory(Long categoryId, Pageable pageable) {
+
+        Category category = categoryServiceApi.findByIdWithProducts(categoryId)
+                .orElseThrow(() -> new GlobalException(CategoryErrorCode.CATEGORY_NOT_FOUND));
+
+        Page<Product> productPage = productServiceApi.findProductsByCategoryId(categoryId, pageable);
+
+        Page<ProductSimpleResponse> productDtoPage = productPage.map(ProductSimpleResponse::from);
+
+        return CategoryProductResponse.from(category,productDtoPage);
+    }
+}
